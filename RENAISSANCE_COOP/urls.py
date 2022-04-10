0462676aff5ef29f941cf5a1661056854a73de2c
kevin67890 <98192943+kevin67890@users.noreply.github.com>
2022-04-10 14:19:23 -0400

    Add files via upload

--- RENAISSANCE_COOP/urls.py
+++ RENAISSANCE_COOP/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("register", views.register, name="register"),
     path("logout", views.logout, name="logout"),
+    path("create", views.create, name="create"),
 ]
--- RENAISSANCE_COOP/views.py
+++ RENAISSANCE_COOP/views.py
@@ -1,4 +1,4 @@
-from email import message
+# from email import message
 from django.shortcuts import redirect, render
 from RENAISSANCE_COOP.models import Produit
 from django.contrib.auth.models import User, auth
@@ -22,13 +22,13 @@ def login(request):
         username = request.POST["username"]
         password = request.POST["password"]
 
-        user = auth.authenticate(username=username,password=password)
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
             return redirect("/")
         else:
-            messages.info(request,"mauvais mot de pass ou nom d'utilisateur")
+            messages.info(request, "mauvais mot de pass ou nom d'utilisateur")
             return redirect("login")
 
     else:
@@ -45,18 +45,21 @@ def register(request):
 
         if password1 == password2:
             if User.objects.filter(username=username).exists():
+                print("nom deja pris")
                 messages.info(request, "Nom déjà pris")
                 return redirect("register")
             elif User.objects.filter(email=email).exists():
+                print("email deja pris")
                 messages.info(request, "Email déjà pris")
                 return redirect("register")
             else:
                 user = User.objects.create_user(
                     username=username, password=password1, email=email)
                 user.save()
-                print("user created")
+                print("compte créé")
                 return redirect("login")
         else:
+            print("password pas correct")
             messages.info(request, "Password pas correct")
             return redirect("register")
         return redirect("/")
@@ -64,6 +67,24 @@ def register(request):
     else:
         return render(request, "register.html")
 
+
 def logout(request):
     auth.logout(request)
     return redirect("/")
+
+
+def create(request):
+    
+    if request.method == "POST":
+
+        produits = Produit()
+        produits.name = request.POST["nom"]
+        produits.img = request.POST["image"]
+        produits.desc = request.POST["description"]
+        produits.price = request.POST["prix"]
+        
+        produits.save()
+        return redirect("/")
+
+    else:
+        return render(request, "create.html")
